bdd7ea867219ec50799435ec0b8aefdafdf1e762
Rob B <2155569b@student.gla.ac.uk>
2016-09-01 13:51:19 +0000

    changed app setup

--- pizza_face_project/pizza_face_project/settings.py
+++ pizza_face_project/pizza_face_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pizza_ml.apps.PizzaMlConfig',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ pizza_face_project/pizza_ml/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PizzaMlConfig(AppConfig):
+    name = 'pizza_ml'
